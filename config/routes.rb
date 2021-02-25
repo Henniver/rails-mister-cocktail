f56541e45ae3b416a48f3dfc4a13567ee6e0b724
Hendrik Vermeersch <hendrik.vermeersch@protonmail.com>
2021-02-25 19:35:13 +0300

    FEAT: cocktails show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # A user can see the list of cocktails
+  # A user can see the details of a given cocktail, with the dose needed for each ingredient
+  # GET "cocktails/42"
+  # A user can create a new cocktail
+  # GET "cocktails/new"
+  # POST "cocktails"
+  resources :cocktails, only: [:index, :new, :create, :show] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: :destroy
 end
